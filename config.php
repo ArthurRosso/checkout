6c8d8790fc38666dd50fc414df18f924869abd7e
ArthurRosso <arthurderosso@gmail.com>
2020-02-17 10:04:07 -0300

    add create table part 3

--- config.php
+++ config.php
@@ -13,7 +13,18 @@ $sql = "SELECT * FROM PRODUCTS";
 if($link->query($sql)){
 
 } else {
-    $sql = "CREATE TABLE `checkout`.`PRODUCTS` ( `_id` INT(32) NOT NULL AUTO_INCREMENT , `prodName` VARCHAR(32) NOT NULL , `prodBrand` VARCHAR(32) NOT NULL , `prodDescription` VARCHAR(32) NOT NULL , `prodProvider` VARCHAR(32) NOT NULL , `authorName` VARCHAR(32) NOT NULL , `prodAmount` INT(32) NOT NULL , `prodUnit` VARCHAR(32) NOT NULL , `prodPrice` VARCHAR(32) NOT NULL , `prodResource` VARCHAR(32) NOT NULL , PRIMARY KEY (`_id`))"
+    $sql = "CREATE TABLE PRODUCTS ( 
+        _id serial PRIMARY KEY, 
+        prodName VARCHAR (50) NOT NULL, 
+        prodBrand VARCHAR (32) NOT NULL, 
+        prodDescription VARCHAR (32) NOT NULL, 
+        prodProvider VARCHAR (32) NOT NULL, 
+        authorName VARCHAR (32) NOT NULL, 
+        prodAmount INTEGER (32) NOT NULL, 
+        prodUnit VARCHAR (32) NOT NULL, 
+        prodPrice VARCHAR (32) NOT NULL, 
+        prodResource VARCHAR (32) NOT NULL 
+    )"
     $link->query($sql);
 }
 
