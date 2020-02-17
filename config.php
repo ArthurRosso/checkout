095c61c6004d8a1c7d38c56da00380777c24de79
ArthurRosso <arthurderosso@gmail.com>
2020-02-17 10:30:50 -0300

    add create table part 7

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 # This function reads your DATABASE_URL config var and returns a connection
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
-    extract(parse_url($_ENV["DATABASE_URL"]));
+    extract(parse_url("postgres://gjlnaecixevmhz:e1c132e283ab992399c1ed7af356c64cb89f6092ad3dc6bd9b57cb66c6c44af9@ec2-50-17-178-87.compute-1.amazonaws.com:5432/d2tp2kgcaicg2g"));
     return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
   
