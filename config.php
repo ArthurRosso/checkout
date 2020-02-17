415c3194ab89ab119937d2453c68141ec15e4f0f
ArthurRosso <arthurderosso@gmail.com>
2020-02-17 09:56:36 -0300

    add create table part 2

--- config.php
+++ config.php
@@ -14,6 +14,7 @@ if($link->query($sql)){
 
 } else {
     $sql = "CREATE TABLE `checkout`.`PRODUCTS` ( `_id` INT(32) NOT NULL AUTO_INCREMENT , `prodName` VARCHAR(32) NOT NULL , `prodBrand` VARCHAR(32) NOT NULL , `prodDescription` VARCHAR(32) NOT NULL , `prodProvider` VARCHAR(32) NOT NULL , `authorName` VARCHAR(32) NOT NULL , `prodAmount` INT(32) NOT NULL , `prodUnit` VARCHAR(32) NOT NULL , `prodPrice` VARCHAR(32) NOT NULL , `prodResource` VARCHAR(32) NOT NULL , PRIMARY KEY (`_id`))"
+    $link->query($sql);
 }
 
 ?>
