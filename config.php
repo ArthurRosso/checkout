1e5fdfed972e95bf4dc7262f07428d911e5e3443
ArthurRosso <arthurderosso@gmail.com>
2020-02-20 16:21:17 -0300

    update database credentials

--- config.php
+++ config.php
@@ -6,11 +6,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 /* Attempt to connect to PostgreSQL database */
-$host = "ec2-184-72-236-57.compute-1.amazonaws.com";
+$host = "ec2-3-229-210-93.compute-1.amazonaws.com";
 $port = "5432";
-$dbname = "d1akuuj1dkvst3";
-$user = "essqjadckawcxv";
-$passwd = "228a4c2c945b06175a7ef73db1ae99737dc53aac98a383571b58794c0a4c56ae";
+$dbname = "d4t5dfjn1fje71";
+$user = "yqiffimujhschw";
+$passwd = "2daf879fd5d1a935951c7b287723b563a11f16043b01fcc616e0fa5566805ad0";
 $con_string = "host=$host port=$port dbname=$dbname user=$user password=$passwd sslmode=require";
 $link = pg_connect($con_string);
  
